58483083bc2b856dd802084fe3408e7457293ce6
Sengolda <79252176+Sengolda@users.noreply.github.com>
2021-10-23 21:51:09 +0530

    Added the src.

--- hard_lang.py
+++ hard_lang.py
@@ -1,1 +1,12 @@
-# TODO: make this
+import sys
+
+if len(sys.argv) == 2:
+    with open(sys.argv[1], "r", encoding="utf-8") as f:
+        lines = f.read().split("\n")
+        if lines[-1:] == ["---end---"]:
+            for x, y in enumerate(lines, start=1):
+                if x == 1 and y == "---start---":
+                    for line in lines:
+                        if line.startswith("output"):
+                            l = line.split(" ")
+                            print(l[1])
